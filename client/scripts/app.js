782bd7a1b25f4acc84885edee06708d473ac54c9
dyliman <dyyliman@gmail.com>
2018-06-22 16:33:37 -0700

    Modified render message to include multiple elements: text, username.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,108 @@
-// YOUR CODE HERE:
+var app = { 
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  
+  init: function() {
+    $('.username').on('click', function(event) {
+      app.handleUsernameClick();
+    });
+  },
+  
+  send: function(message){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  
+  fetch: function(){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  
+  clearMessages: function(){
+    $('#chats').children().remove();
+  },
+  
+  renderMessage: function(message) {
+    var messageBox = $('<div></div>');
+    messageBox.addClass('message');
+    var userBox = $('<a>');
+    userBox.addClass('username');
+    var textBox = $('<p>');
+    textBox.addClass('text');
+    
+    textBox.text(message.text);
+    messageBox.append(userBox);
+    messageBox.append(textBox);
+    
+    $('#chats').append(messageBox);
+  },
+  
+  renderRoom: function(room){
+    var div = $('<div>');
+    var divClass = room;
+    div.addClass(divClass);
+    $('#roomSelect').append(div);
+  },
+  
+  handleUsernameClick: function() {
+    
+  },
+  
+  handleSubmit: function() {
+    
+  }
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
