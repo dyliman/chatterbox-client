47c61e85caa77f896c44e16e8bd051aa5d75b14a
dyliman <dyyliman@gmail.com>
2018-06-23 13:30:26 -0700

    Room names load correctly (per our decision).

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,45 @@
 $( document ).ready(function() {
   var app = { 
     server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
-
+    uniqRoom: new Set(),
+    
+    
     init: function() {
+
       $('.username').on('click', function(event) {
         app.handleUsernameClick();
       });
     
       //$('#send .submit').submit()
-      $('#send .submit').on('submit', app.handleSubmit());
+      $('#button').click(function(){
+      console.log("clicked")
+      app.handleSubmit();
+      });
+
+      $('#roomButton').click(function() {
+        console.log('making a room');
+        var roomText = $('#createRoom').val();
+        // JSON.stringify(roomText)
+        // roomText = roomText.replace(/[\W_]+/g, ' ');
+        // var option = $('<option>');
+        // option.text(roomText);
+        // option.val(roomText);
+        app.uniqRoom.add(roomText);
+        app.renderRoom();
+        // get the text value
+        // create some kind of element
+        // set its text as the input from the form.
+        // use append  to add it as a child of the roomselect id.
+      })
+// $( "#target" ).submit(function( event ) {
+//   alert( "Handler for .submit() called." );
+//   event.preventDefault();
+// });
+
+// $( "#other" ).click(function() {
+//   $( "#target" ).submit();
+// });
+
     },
   
     send: function(message) {
@@ -63,22 +94,45 @@ $( document ).ready(function() {
       var userBox = $('<a>');
       userBox.addClass('username');
       
+      var roomBox = $('<a>');
+      roomBox.addClass('roomName');
+      
+      var span = $('<span>');
+      
       var textBox = $('<p>');
       textBox.addClass('text');
       
       userBox.text(message.username.replace(/[\W_]+/g, ' '));
       textBox.text(message.text.replace(/[\W_]+/g, ' '));
-      messageBox.append(userBox);
+      
+      
+      span.append(userBox);
+      
+      if(message.roomname !== undefined){
+        roomBox.text('Room name: ' + message.roomname.replace(/[\W_]+/g, ' '));
+        span.append(roomBox);
+        app.uniqRoom.add(message.roomname)
+      }
+      
+      messageBox.append(span)
       messageBox.append(textBox);
       
       $('#chats').append(messageBox);
+      
+      
+      
     },
   
-    renderRoom: function(room) {
-      var div = $('<div>');
-      var divClass = room;
-      div.addClass(divClass);
-      $('#roomSelect').append(div);
+    renderRoom: function() {
+      $('#roomSelect').empty();
+      
+      app.uniqRoom.forEach(function(room){
+        var option = $('<option>');
+        option.text(room.replace(/[\W_]+/g, ' '));
+        $('#roomSelect').append(option);
+      });
+      
+      //$('#roomSelect').append(option);
     },
     
     handleUsernameClick: function() {
@@ -86,6 +140,19 @@ $( document ).ready(function() {
     },
     
     handleSubmit: function() {
+      var text = $('#textMessage').val();
+      // var name = $('#nameInput').val();
+      var message = {};
+      message.text = text;
+      message.username = window.location.search;
+      
+      var room = $('#roomSelect').val();
+        if(room !== "Default" && room !== undefined){
+          message.roomname = room;
+        }
+        
+      // message.username = "<script>app.send({text: \"Check yo' console, sucka!\"}); console.log(\"You sent this\")</script>";
+      app.send(message);
       console.log('inside the handleSubmit method!!!');
     },
 
@@ -97,7 +164,11 @@ $( document ).ready(function() {
     setInterval(function() {
       app.fetch();
     }, 2000);
+    // setInterval(function() {
+    //   app.init();
+    // }, 2000);
   };
+  app.init();
   app.fetch();
   refresh();
 
